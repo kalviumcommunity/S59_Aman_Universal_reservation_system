1b77871fee95c27a2e9d77753f099cde431c1674
hiamanagrawal <hiamanagrawal@gmail.com>
2024-08-11 23:19:21 +0530

    added array of objects

--- src/Place.cpp
+++ src/Place.cpp
@@ -1,26 +1,15 @@
 #include "Place.h"
-#include <iostream>
-
+#include <iostream> 
 using namespace std;
 
 Place::Place(const string &name, int totalSeats) : name(name), totalSeats(totalSeats) {}
 
 void Place::displayInfo() const
 {
-    cout << "Place: " << this->name << ", Total Seats: " << this->totalSeats << endl;
+    cout << "Place: " << name << ", Total Seats: " << totalSeats << endl;
 }
 
 bool Place::checkAvailability() const
 {
-    return this->totalSeats > 0;
-}
-
-void Place::setName(const string &name)
-{
-    this->name = name;
-}
-
-void Place::setTotalSeats(int totalSeats)
-{
-    this->totalSeats = totalSeats;
+    return totalSeats > 0;
 }
